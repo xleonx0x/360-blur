095664202734a2d53aa77d8519ee13e60ad0a2d2
Elvene <123799877+xleonx0x@users.noreply.github.com>
2024-07-06 19:11:12 +1000

    stuff

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/360-blur/",
-  plugins: [react()]
-})
